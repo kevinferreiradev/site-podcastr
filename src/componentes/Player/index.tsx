29fb06c36d24bdaf45990ad30c70a6ab4c572e99
kevin ferreira <ferreirakevin325@gmail.com>
2021-04-25 11:42:34 -0300

    contrução do botton onClic

--- src/componentes/Player/index.tsx
+++ src/componentes/Player/index.tsx
@@ -3,14 +3,16 @@ import styles from './styles.module.scss';
 import { PlayerContext } from '../../Contexts/PlayerContext';  
  
 export function Player () {
-  const player = useContext(PlayerContext)
+  const {  episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+
+  const episode = episodeList[currentEpisodeIndex]
 
 
   return (
     <div className={styles.playerContainer}>
       <header>
         <img src="/playing.svg" alt="Tocando agora" />
-        <strong>Tocando agora {player}</strong>
+        <strong>Tocando agora {episode?.title}</strong>
       </header>
 
       <div className={styles.emptyPlayer}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,21 @@ import { PlayerContext } from '../Contexts/PlayerContext';
 import '../styles/global.scss'
 
 import styles from '../styles/app.module.scss';
+import { useState } from 'react';
 
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
   return (
-    <PlayerContext.Provider value={'Diego'}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
         <div className={styles.wrapper}>
           <main>
             <Header />
